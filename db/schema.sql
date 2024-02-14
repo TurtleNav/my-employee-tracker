5cec8e23aab6ede07630224f0c23860efb51aa2a
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-14 11:54:58 -0500

    reformated seed data and table name

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,31 @@ CREATE DATABASE business_db;
 
 USE business_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES departments(id)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES roles(id)
+    REFERENCES role(id)
     ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
-    REFERENCES employees(id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,25 @@
-INSERT INTO departments (name)
+INSERT INTO department (name)
 VALUES ("Engineering"),
        ("Finance"),
        ("Legal"),
        ("Sales");
 
-INSERT INTO roles (title, department, salary)
-VALUES ("Sales Lead", 4, 100000);
-VALUES ("Salesperson", 4, 80000);
-VALUES ("Lead Engineer", 1, 150000);
-VALUES ("Software Engineer", 1, 120000);
-VALUES ("Account Manager", 2, 160000);
-VALUES ("Accountant", 2, 125000);
-VALUES ("Legal Team Lead", 3, 250000);
-VALUES ("Lawyer", 3, 190000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4),
+       ("Salesperson", 80000, 4),
+       ("Lead Engineer", 150000, 1),
+       ("Software Engineer", 120000, 1),
+       ("Account Manager", 160000, 1),
+       ("Accountant", 125000, 2),
+       ("Legal Team Lead", 250000, 3),
+       ("Lawyer", 190000, 3);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, NULL);
-VALUES ("Mike", "Chan", 2, 1);
-VALUES ("Ashely", "Rodriguez", 3, NULL);
-VALUES ("Kevin", "Tupik", 4, 3);
-VALUES ("Kunal", "Singh", 5, NULL);
-VALUES ("Malia", "Brown", 6, 5);
-VALUES ("Sarah", "Lourd", 7, NULL);
-VALUES ("Tom", "Allen", 8, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, NULL),
+       ("Mike", "Chan", 2, 1),
+       ("Ashely", "Rodriguez", 3, NULL),
+       ("Kevin", "Tupik", 4, 3),
+       ("Kunal", "Singh", 5, NULL),
+       ("Malia", "Brown", 6, 5),
+       ("Sarah", "Lourd", 7, NULL),
+       ("Tom", "Allen", 8, 7);
